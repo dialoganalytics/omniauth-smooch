a03044575e4b5a088d4494d615b3727670c58117
Philippe Dionne <dionne.phil@gmail.com>
2016-09-11 12:49:09 -0400

    Configure strategy with smooch infos

--- lib/omniauth/strategies/smooch.rb
+++ lib/omniauth/strategies/smooch.rb
@@ -1,8 +1,15 @@
-require 'omniauth'
+require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
-    class Smooch
+    class Smooch < OmniAuth::Strategies::OAuth2
+      option :name, 'smooch'
+
+      option :client_options, {
+        site: "https://smooch.io/",
+        authorize_url: "https://app.smooch.io/oauth/authorize",
+        token_url: "https://app.smooch.io/oauth/token"
+      }
     end
   end
 end
